b821a1e5954f554439a487494ee48666a2fcc6cb
amankumar89 <amankumaroo784@gmail.com>
2026-06-27 12:19:45 +0530

    added auth user schema

---
+++ server/src/db/schema.ts
@@ -0,0 +1,41 @@
+import { sql } from "drizzle-orm";
+import {
+  pgTable,
+  timestamp,
+  pgEnum,
+  serial,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const roles = ["USER", "ADMIN", "TEACHER"] as const;
+export const userRoleEnum = pgEnum("user_role", roles);
+export type UserRole = (typeof roles)[number];
+
+export const status = ["ACTIVE", "INACTIVE", "BLOCKED", "DELETED"] as const;
+export const userStatusEnum = pgEnum("user_status", [
+  "ACTIVE",
+  "INACTIVE",
+  "BLOCKED",
+  "DELETED",
+]);
+export type UserStatus = (typeof status)[number];
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: varchar("name").notNull(),
+  email: varchar("email").notNull().unique(),
+  password: varchar("password", { length: 255 }).notNull(),
+  role: userRoleEnum("role").default("USER").notNull(),
+  status: userStatusEnum("status").default("ACTIVE").notNull(),
+  lastLogin: timestamp("last_login", { withTimezone: true }),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .defaultNow()
+    .notNull(),
+  updatedAt: timestamp("updated_at")
+    .defaultNow()
+    .$onUpdateFn(() => sql`now()`)
+    .notNull(),
+});
+
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
